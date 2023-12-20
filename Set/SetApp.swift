6de1480e6490e7ce0d6cc274e447fdfb3aee61eb
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2023-12-20 13:52:57 +0100

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Kristina Grebneva on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
